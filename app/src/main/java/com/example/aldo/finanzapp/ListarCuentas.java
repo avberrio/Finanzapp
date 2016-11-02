e14438a18e9febf4c9672f86954688881de16ba0
avberrio <aldoberrios94@gmail.com>
2016-11-02 08:07:00 -0300

    Creating project

---
+++ app/src/main/java/com/example/aldo/finanzapp/ListarCuentas.java
@@ -0,0 +1,13 @@
+package com.example.aldo.finanzapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListarCuentas extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_cuentas);
+    }
+}
