4c53b0f741f7f6905a0cbe782229dcf7d352ce51
avberrio <aldoberrios94@gmail.com>
2016-11-11 01:51:33 -0300

    Toolbar buttons added

--- app/src/main/java/com/example/aldo/finanzapp/ListarCuentas.java
+++ app/src/main/java/com/example/aldo/finanzapp/ListarCuentas.java
@@ -1,13 +1,47 @@
 package com.example.aldo.finanzapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class ListarCuentas extends Activity {
+public class ListarCuentas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_cuentas);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //myToolbar.setTitle("Dashboard");
+        //myToolbar.setTitleTextColor(Color.WHITE);
+        myToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24px);   /* Back button in left of action bar */
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
     }
 }
