8793c1e2591c916d71bfc3b2c856b5b03b4eff4f
avberrio <aldoberrios94@gmail.com>
2016-11-14 22:48:38 -0300

    DatePicker implemented in AddExpenseveActivity

--- app/src/main/java/com/example/aldo/finanzapp/activities/ListarCuentas.java
+++ app/src/main/java/com/example/aldo/finanzapp/activities/ListarCuentas.java
@@ -30,19 +30,10 @@ public class ListarCuentas extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        /*
-        billsList = new ArrayList<Bills>();
-        billsList.add(new Bills("Electricidad", 20000, "Viernes 28 de Octubre de 2016"));
-        billsList.add(new Bills("Agua", 15758, "Sabado 30 de Octubre de 2016"));
-        billsList.add(new Bills("Arriendo", 200000, "Viernes 4 de Noviembre de 2016"));
-        billsList.add(new Bills("Plan de Teléfono", 15990, "Viernes 11 de Noviembre de 2016"));
-        billsList.add(new Bills("Otros", 20000, "Viernes 28 de Octubre de 2016"));
-        billsList.add(new Bills("Más", 15758, "Sabado 30 de Octubre de 2016"));
-        */
-        bill = new Bills("Más", "15758", "Sabado 30 de Octubre de 2016", "Hola mundo");
+        //bill = new Bills("Más", "15758", "Sabado 30 de Octubre de 2016", "Hola mundo");
         billsDAO = new BillsDAO(this);
         billsDAO.open();
-        billsDAO.createBill(bill);
+        //billsDAO.createBill(bill);
         billsList = billsDAO.getAllTasks();
 
         newAdapter = new MyClassAdapter(this, billsList);
